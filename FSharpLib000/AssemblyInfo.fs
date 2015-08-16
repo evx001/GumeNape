4e2e9c4e5cd573fbd0b99003dcfbf1de9d6e9751
evx001 <eviexeris@gmail.com>
2015-08-16 16:43:16 -0400

    FSharpLiB

---
+++ FSharpLib000/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpLib000
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpLib000")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("phunctor.com")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("evxyz")>]
+[<assembly: AssemblyTrademark("phunctorTM")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ FSharpLib000/Component1.fs
@@ -0,0 +1,5 @@
+﻿namespace FSharpLib000
+
+type Class1() = 
+    member this.X = "F#"
+
